149314ab31705590051516940d38d0c84a10c8ed
Christophe Verclytte <1611574+chris-verclytte@users.noreply.github.com>
2020-08-13 15:30:38 +0200

    Clean forgotten print traces (#3)

--- ghibli/api/routes/films/list.py
+++ ghibli/api/routes/films/list.py
@@ -6,6 +6,5 @@ import ghibli.services.ghibli.films as films
 
 def list_films(*, with_people=False):
     """List all Ghibli films"""
-    print("FILMS = ", films)
     films_list = films.list_films_with_people() if with_people else films.list_films()
     return {"films": films_list}, HTTPStatus.OK
