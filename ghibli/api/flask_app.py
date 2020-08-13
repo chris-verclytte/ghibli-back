0f87bdaad0b71680335b30d78c3237458df5d573
Christophe Verclytte <1611574+chris-verclytte@users.noreply.github.com>
2020-08-13 12:06:49 +0200

    Add CORS

--- ghibli/api/flask_app.py
+++ ghibli/api/flask_app.py
@@ -1,5 +1,6 @@
-"""Return an initialized Flask application with the API."""
+"""Initialization module for Flask application registering the API."""
 from flask import Flask
+from flask_cors import CORS  # type: ignore
 
 from ghibli.api.api import create_api
 
@@ -7,6 +8,7 @@ from ghibli.api.api import create_api
 def create_app() -> Flask:
     """Initialize a Flask app."""
     app = Flask(__name__)
+    CORS(app)
 
     api = create_api()
     app.register_blueprint(api)
